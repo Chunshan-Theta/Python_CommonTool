6112b5dd7550c9b1ca73e813b450353f063d0a38
Wang <gavin19950511@gmail.com>
2017-01-25 11:54:53 +0800

    Update json.py

--- json.py
+++ json.py
@@ -1,13 +1,28 @@
 #-*-coding:utf-8-*-
 import json
 
+#read json file 
 f = open("JsonData.json", 'r')
 b_str = f.read()
 f.close()
+
+#loading json
 GpioConfig = json.loads(b_str,encoding='utf-8')
-t='早安'
-u = unicode(t, "utf-8")
-print type(u)
+
+#call data using chinese key
+u = unicode('早安', "utf-8")
+print GpioConfig[u]
+
+# print all key and data
+for key, value in GpioConfig.iteritems():
+    print key,type(key)
+    print GpioConfig[u]
+    
+# conbine two array
+
+GpioConfig_b  = json.loads('{"安安":"哈摟"}')
+c = dict(GpioConfig_a.items() + GpioConfig_b.items())
+
 for key, value in GpioConfig.iteritems():
     print key,type(key)
     print GpioConfig[u]
